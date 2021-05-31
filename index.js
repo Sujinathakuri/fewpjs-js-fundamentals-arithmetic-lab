eb604dbd2d1c1fa47eb040b9354c78df8672c05a
Sujinathakuri <sujchand2@gmail.com>
2021-05-31 12:13:22 -0400

    commit tree 737db4399a9884aaca0fd49a4e9469d867a0f243

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 // Write your code here
 
-const num1 = 31
-const num2 = 2
-const multiply = num1 * num2
-
-const random = Math.floor(Math.random() * 10)
-
-const num3 = 14
-const num4 = 5
-const mod = num3 % num4
-
-const max = Math.max(20, 10)
